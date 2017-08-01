4c9fc9f7448cc91891b6f567cb9e45f4c384d540
kavinkumar06 <kavinm09@gmail.com>
2017-08-01 17:25:10 +0530

    Create Holiday.java

---
+++ Holiday.java
@@ -0,0 +1,18 @@
+# Holiday
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/
+class Day
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner obj=new Scanner(System.in);
+		String j=obj.nextLine();
+		if((j.equals("Monday"))||(j.equals("Tuesday"))||(j.equals("Wednesday"))||(j.equals("Thursday"))||(j.equals("Friday))))
+		System.out.println("Working day");
+		else
+		System.out.println("Holiday");
+	}
+}
